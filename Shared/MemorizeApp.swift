0b06435e5211c47d97507064db52e02740c59438
Bruno Roja <brunojlee@hotmail.com>
2022-07-16 13:51:46 -0400

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Lee on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
